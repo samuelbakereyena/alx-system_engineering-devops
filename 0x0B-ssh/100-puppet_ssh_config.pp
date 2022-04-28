72139e601fab3ed7826775b07d39921c029d1645
Eyena Samuel Baker <samuelbaker.ey@gmail.com>
2022-04-28 14:28:48 +0300

    create an SSH RSA key pair

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# setting up a client SSH configeration file so that we can connect to server without using a password
+
+include stdlib
+file_line{ 'Declare identity file':
+path 	=> '/etc/ssh/ssh_config',
+line    => '    IdentityFile ~/.ssh/holberton',
+replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+path	=>'/etc/ssh/ssh_config',
+line	=>'	PasswordAuthentication no',
+replace =>true,
+}
