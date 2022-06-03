f2ed37d1a52f8f760a1c03ab18dfc10d283ad626
Eyena Samuel Baker <samuelbaker.ey@gmail.com>
2022-06-03 14:19:41 +0300

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Creating a file in the /tem directory
-
-file { '/temp/hoberton':
-ensure	=> 'flle',
-mode	=> '0744',
-owner	=> 'www-data',
-group	=> 'www-data',
-content	=> 'I love Puppet',
+file { '/tmp/holberton':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
